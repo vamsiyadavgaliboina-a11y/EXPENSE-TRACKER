69b58fa66e5cca3945345a0dc377b281f49408d8
Vamsi <vamsiyadavgaliboina@gmail.com>
2026-09-09 13:53:18 +0530

    update

--- server/api/index.js
+++ server/api/index.js
@@ -2,6 +2,18 @@ import app from '../server.js';
 import connectDB from '../config/db.js';
 
 export default async function handler(req, res) {
+  const origin = req.headers.origin;
+  if (origin === 'https://expense-tracker-ochre-xi-58.vercel.app') {
+    res.setHeader('Access-Control-Allow-Origin', origin);
+    res.setHeader('Access-Control-Allow-Credentials', 'true');
+  }
+
+  if (req.method === 'OPTIONS') {
+    res.setHeader('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    return res.status(204).end();
+  }
+
   await connectDB();
   return app(req, res);
 }
